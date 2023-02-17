8f945336a4bdd7fee6b79864ccefe591d482cb79
Jeon <dbs_dmswl92@naver.com>
2023-02-17 16:24:53 +0900

    add- Shader file : blendType, DepthStencil Type

--- Engine/Shader.h
+++ Engine/Shader.h
@@ -5,6 +5,7 @@ enum class SHADER_TYPE : uint8
 {
 	DEFFERED,
 	FORWARD,
+	LIGHTING,
 };
 
 enum class RASTERIZER_TYPE : uint8
@@ -22,6 +23,17 @@ enum class DEPTH_STENCIL_TYPE : uint8
 	LESS_EQUAL,			
 	GREATER,
 	GREATER_EQUAL,
+	NO_DEPTH_TEST,			//깊이 테스트x, 깊이 기록 o
+	NO_DEPTH_TEST_NO_WRITE,	//깊이 테스트x, 깊이 기록 o
+	LESS_NO_WRITE,			//깊이 테스트o, 깊이 기록 x
+};
+
+enum class BLEND_TYPE : uint8
+{
+	DEFAULT,
+	ALPHA_BLEND,		//투명도를 이용한 블랜드
+	ONE_TO_ONE_BLEND,
+	END,
 };
 
 struct ShaderInfo
@@ -30,6 +42,8 @@ struct ShaderInfo
 	SHADER_TYPE shaderType = SHADER_TYPE::FORWARD;
 	RASTERIZER_TYPE rasterizerType = RASTERIZER_TYPE::CULL_BACK;
 	DEPTH_STENCIL_TYPE depthStencilType = DEPTH_STENCIL_TYPE::LESS;
+	BLEND_TYPE blendType = BLEND_TYPE::DEFAULT;
+
 	D3D12_PRIMITIVE_TOPOLOGY_TYPE topologyType = D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE;
 };
 
@@ -50,7 +64,13 @@ public:
 
 public:
 
-	void Init(const wstring& path, ShaderInfo info = ShaderInfo());
+	void Init(
+		const wstring& path,
+		ShaderInfo info = ShaderInfo(),
+		const string& vs = "VS_Main",
+		const string& ps = "PS_Main"
+		);
+
 	void Update();
 
 	SHADER_TYPE GetShaderType() { return _info.shaderType; }
